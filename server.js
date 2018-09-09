8a636f6e6c648af5a603b7f80fcf0f4cb782a68d
Jasmin Trotter <jasminmtrotter@gmail.com>
2018-09-09 10:43:30 -0700

    users / auth endpoints

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
- const express = require('express');
- const app = express();
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
 
- const PORT = process.env.PORT || 3000;
+// Here we use destructuring assignment with renaming so the two variables
+// called router (from ./users and ./auth) have different names
+// For example:
+// const actorSurnames = { james: "Stewart", robert: "De Niro" };
+// const { james: jimmy, robert: bobby } = actorSurnames;
+// console.log(jimmy); // Stewart - the variable name is jimmy, not james
+// console.log(bobby); // De Niro - the variable name is bobby, not robert
+const { router: usersRouter } = require('./users');
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL } = require('./config');
 
 const cors = require('cors');
 const {CLIENT_ORIGIN} = require('./config');
@@ -12,10 +30,70 @@ app.use(
     })
 );
 
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use('/api/users/', usersRouter);
+app.use('/api/auth/', authRouter);
+
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
  app.get('/api/*', (req, res) => {
    res.json({ok: true});
  });
 
- app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.use('*', (req, res) => {
+  return res.status(404).json({ message: 'Not Found' });
+});
 
- module.exports = {app};
+// A protected endpoint which needs a valid JWT to access it
+app.get('/api/protected', jwtAuth, (req, res) => {
+  return res.json({
+    data: 'rosebud'
+  });
+});
+
+ // Referenced by both runServer and closeServer. closeServer
+// assumes runServer has run and set `server` to a server object
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+// app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+ module.exports = { app, runServer, closeServer };
